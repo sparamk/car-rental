11606d38b8173f8161ada7b9b4d84a5ccd80b237
SHASHANK MARAGANI <shashankmaragani@gmail.com>
2022-12-04 16:58:39 -0600

    Auto stash before rebase of "origin/develop"

---
+++ appointment/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from car.views import *
+from sales.views import *
+
+# Create your views here.
+def scheduleTestDrive(request):
+    pass
+
+def scheduleAppointment(request):
+    pass
