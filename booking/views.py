568164dc3751bffbd064e819986e02bc0c157dd6
sruthikalidindi <skalidi@ilstu.edu>
2022-12-04 17:21:29 -0600

    Added booking and transaction modules.

---
+++ booking/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from feedback.views import *
+from accident.views import *
+from transaction.views import *
+
+# Create your views here.
+def fareEstimator(request):
+    pass
+
+def editBooking(request):
+    pass
+
+def cancelBooking(request):
+    pass
+
+def retrieveBooking(request):
+    pass
+
+def rescheduleBooking(request):
+    pass
+
+def bookCar(request):
+    pass
+
+def upgradeCar(request):
+    pass
