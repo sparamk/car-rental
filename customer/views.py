041dfb352cd8d8d093b49455192dbc9680857b08
manideep <mkasire@ilstu.edu>
2022-12-04 15:43:02 -0600

    adding html page templates

---
+++ customer/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from user.views import *
+from booking.views import *
+
+# Create your views here.
+def digitalCheckin(request):
+    pass
+
+def referFriend(request):
+    pass
+
+def myWallet(request):
+    pass
