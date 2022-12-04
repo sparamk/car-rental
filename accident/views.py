27747537c4d30c90e6f90f789148fefb7c97e9f1
Shashaank Reddy gurrala <sgurra2@ilstu.edu>
2022-12-04 17:11:49 -0600

    added manager and accident modules

---
+++ accident/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def reportAccident(request):
+    pass
+
+def roadSideAssistance(request):
+    pass
+
+def vehicleTheftReport(request):
+    pass
---
+++ manager/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from employee.views import *
+
+# Create your views here.
+def addEmployee(request):
+    pass
+
+def editEmployee(request):
+    pass
+
+def deleteEmployee(request):
+    pass
+
+def editCarAvailability(request):
+    pass
+
+def deleteBooking(request):
+    pass
+   
+def ApproveRefund(request):
+    pass
+   
