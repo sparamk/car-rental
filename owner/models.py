b2d63d7662ed8116bb8da447ba0417fa6777b5ce
Shashaank Reddy gurrala <sgurra2@ilstu.edu>
2022-12-06 20:04:28 -0600

    Adding owner module

--- owner/models.py
+++ owner/models.py
@@ -7,9 +7,12 @@ class Cars(models.Model):
     car_name = models.CharField(max_length = 20)
     color = models.CharField(max_length = 10)
     capacity = models.CharField(max_length = 2)
+    rent_per_hour = models.CharField(max_length=6, default=1)
+    cost_of_vehicle = models.CharField(max_length=6, default=2000)
     is_available = models.BooleanField(default = True)
     description = models.CharField(max_length = 100)
-    car_condition = models.BooleanField()
+    car_condition = models.BooleanField(default=1)
+    subscription = models.CharField(max_length=6,default='1')
     date_added = models.DateTimeField(auto_now_add=True)
 
 class Coupons(models.Model):
--- owner/views.py
+++ owner/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
 from manager.views import *
-from . models import *
+from .models import *
 from django.contrib.auth.models import User
+from user.views import *
+
 # Create your views here.
 
 
@@ -13,17 +15,22 @@ def addCar(request):
         car.capacity = request.POST['capacity']
         car.description = request.POST['description']
         car.car_condition = request.POST['car_condition']
+        car.subscription = request.POST['subscription']
         car.save()
         success = "The car has been added to the inventory successfully"
-        return render(request, 'addcar.html',{'success':success})
+        return render(request, 'addcar.html', {'success': success})
     return render(request, 'addcar.html')
+
 
 def editCar(request):
     if request.method == 'POST':
         car_id = request.POST['id']
         car = Cars.objects.filter(id=car_id).first()
-        return render(request, 'editcar.html',{'car_id':car.id,'car_name':car.car_name,'color':car.color,'description':car.description,'car_condition':car.car_condition,'capacity':car.capacity,})
+        return render(request, 'editcar.html',
+                      {'car_id': car.id, 'car_name': car.car_name, 'color': car.color, 'description': car.description,
+                       'car_condition': car.car_condition, 'capacity': car.capacity, 'subscription': car.subscription,})
     return render(request, 'editcar.html')
+
 
 def updateCar(request):
     if request.method == 'POST':
@@ -33,14 +40,19 @@ def updateCar(request):
         capacity = request.POST['capacity']
         description = request.POST['description']
         car_condition = request.POST['car_condition']
-        car = Cars.objects.filter(id=car_id).update(car_name=car_name,color=color,capacity=capacity,description=description,car_condition=car_condition)
+        car = Cars.objects.filter(id=car_id).update(car_name=car_name, color=color, capacity=capacity,
+                                                    description=description, car_condition=car_condition)
         success = "The car has been updated successfully"
         car = Cars.objects.filter(id=car_id).first()
-        return render(request, 'editcar.html',{'car_id':car.id,'car_name':car.car_name,'color':car.color,'description':car.description,'capacity':car.capacity,'success':success})
+        return render(request, 'editcar.html',
+                      {'car_id': car.id, 'car_name': car.car_name, 'color': car.color, 'description': car.description,
+                       'capacity': car.capacity, 'success': success,'subscription': car.subscription,})
+
 
 def listCars(request):
     cars = Cars.objects.all()
-    return render(request, 'listcars.html', {'cars':cars})
+    return render(request, 'listcars.html', {'cars': cars})
+
 
 def deleteCar(request):
     if request.method == 'POST':
@@ -50,12 +62,47 @@ def deleteCar(request):
         success = "Car deleted successfully"
         return redirect(listCars)
 
+
+@login_required
+def list_users(request):
+    users_list = []
+    admin = User.objects.filter(is_superuser=True).first()
+    users = User.objects.exclude(username=request.user).exclude(username=admin.username).all()
+
+    for doser in users:
+        l = list(doser.groups.values_list('name', flat=True))
+        user_dictionary = {'username': doser.username, 'first_name': doser.first_name, 'last_name': doser.last_name,
+                           'email': doser.email, 'is_staff': l, 'id': doser.id, 'is_active': doser.is_active}
+        users_list.append(user_dictionary)
+
+    request.session['users_list'] = users_list
+    return render(request, 'list_users.html', {'users': users})
+
+
 def blockUser(request):
-    pass
+    if request.method == 'POST':
+        try:
+            user_id = request.POST['id']
+            user = User.objects.get(id=user_id)
+
+            # Remove user from all groups
+            groups = list(user.groups.values_list('name', flat=True))
+            for group in groups:
+                group.user_set.remove(user)
+
+            # Add user to the blocked group
+            block_group = Group.objects.get(name='block')
+            block_group.user_set.add(user)
+
+            return redirect(list_users)
+        except:
+            return dashboard()
+
 
 def coupons(request):
     coupons = Coupons.objects.all()
-    return render(request, 'coupons.html', {'coupons':coupons})
+    return render(request, 'coupons.html', {'coupons': coupons})
+
 
 def addCoupon(request):
     if request.method == 'POST':
@@ -64,26 +111,32 @@ def addCoupon(request):
         coupon.discount = request.POST['discount']
         coupon.save()
         success = "The coupon has been listed successfully"
-        return render(request, 'addcoupon.html',{'success':success})
+        return render(request, 'addcoupon.html', {'success': success})
     return render(request, 'addcoupon.html')
+
 
 def editCoupon(request):
     if request.method == 'POST':
         coupon_id = request.POST['id']
         coupon = Coupons.objects.filter(id=coupon_id).first()
-        return render(request, 'editcoupon.html',{'coupon_id':coupon.id,'coupon_code':coupon.coupon_code,'discount':coupon.discount,})
+        return render(request, 'editcoupon.html',
+                      {'coupon_id': coupon.id, 'coupon_code': coupon.coupon_code, 'discount': coupon.discount, })
     return render(request, 'editcar.html')
+
 
 def updateCoupon(request):
     if request.method == 'POST':
         coupon_id = request.POST['coupon_id']
         coupon_code = request.POST['coupon_code']
         discount = request.POST['discount']
-        
-        coupon = Coupons.objects.filter(id=coupon_id).update(coupon_code=coupon_code,discount=discount,)
+
+        coupon = Coupons.objects.filter(id=coupon_id).update(coupon_code=coupon_code, discount=discount, )
         success = "The coupon has been updated successfully"
         coupon = Coupons.objects.filter(id=coupon_id).first()
-        return render(request, 'editcoupon.html',{'coupon_id':coupon.id,'coupon_code':coupon.coupon_code,'discount':coupon.discount,'success':success})
+        return render(request, 'editcoupon.html',
+                      {'coupon_id': coupon.id, 'coupon_code': coupon.coupon_code, 'discount': coupon.discount,
+                       'success': success})
+
 
 def deleteCoupon(request):
     if request.method == 'POST':
