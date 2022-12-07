94c0c044ddc0b8aac7d01eb0b7c9dcc629a6ca6f
Shashaank Reddy gurrala <sgurra2@ilstu.edu>
2022-12-06 20:01:28 -0600

    Pushing accident module

--- accident/urls.py
+++ accident/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    # path("", include('user.urls')),
+    path("reportaccident/", views.reportAccident, name='reportaccident'),
+    path("viewaccidentreports/", views.viewAccidentReports, name='viewaccidentreports'),
+    path("roadsideassistance/", views.roadSideAssistance, name='roadsideassistance'),
+    path("viewroadsideassistance/", views.viewRoadSideAssistance, name='viewroadsideassistance'),
+    path("vehicletheftreport/", views.vehicleTheftReport, name='vehicletheftreport'),
+    path("viewvehicletheftreport/", views.viewVehicleTheftReport, name='viewvehicletheftreport'),
 ]
