c04b59bca113d55e383a8b099acc55d6a296250a
ADILSTU\skalidi <skalidi@ilstu.edu>
2022-12-07 04:18:20 -0600

    updates in apointment and customer modules

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    # path("", include('user.urls')),
+    path("scheduletestdrive/", views.scheduleTestDrive, name='scheduletestdrive'),
+    path("scheduleappointment/", views.scheduleAppointment, name='scheduleappointment'),
+    path('appointment/', views.Appointment, name='appointment')
 ]
