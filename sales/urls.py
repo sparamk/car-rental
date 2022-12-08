54de0a98653e055015b60767ba6787fa6463cbd0
ashok <amannam@ilstu.edu>
2022-12-08 16:06:49 +0530

    adding changes in sales,salesanalysis and user modules

--- sales/urls.py
+++ sales/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    # path("sales", include('user.urls')),
+     path('buyVehicle/', views.buyVehicle, name='buyVehicle'),
+     path('sellVehicle/', views.sellVehicle, name='sellVehicle')
+     
 ]
--- user/urls.py
+++ user/urls.py
@@ -12,5 +12,12 @@ urlpatterns = [
     path('deleteaccount/', views.deleteAccount, name="deleteaccount"),
     path('forgotpassword/', views.forgotPassword, name="forgotpassword"),
     path('changeprofilepassword/', views.changeProfilePassword, name="changeprofilepassword"),
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
+    path('success/', views.SuccessView.as_view()),
+    path('cancelled/', views.CancelledView.as_view()),
+    path('webhook/', views.stripe_webhook),
+    path('contactUs/',views.contactUs,name= "contactUs"),
+    path('suggestRoadTrip/',views.suggestRoadTrip,name="suggestRoadTrip")
 ]
 
