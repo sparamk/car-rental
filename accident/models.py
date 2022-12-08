2130b3427d191e28e8000815b558bae01c0016c5
Shashaank Reddy gurrala <sgurra2@ilstu.edu>
2022-12-08 04:40:11 -0600

    Changes added for accident module

--- accident/models.py
+++ accident/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Report_Accident(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
     report_accident = models.TextField()
     accident_date = models.DateTimeField(auto_now_add=True)
 
